40a52d69f2a37c555a940c0261b1f7ee713dbfb2
fptaarsl <talip328@outlook.de>
2024-10-28 16:19:13 +0100

    renamed

--- src/main/java/org/example/anagramchecker/AnagramCheckerApplication.java
+++ src/main/java/org/example/anagramchecker/AnagramCheckerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnagramcheckerApplication {
+public class AnagramCheckerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AnagramcheckerApplication.class, args);
+        SpringApplication.run(AnagramCheckerApplication.class, args);
     }
 
 }
